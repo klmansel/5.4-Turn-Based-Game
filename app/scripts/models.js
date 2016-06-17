42269f4882b35bfc2280bf201467381e76675383
Kara Mansel <callmedevmaybe@gmail.com>
2016-06-16 20:39:05 -0400

    Added progress bars and font. Styled button

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -2,26 +2,30 @@ var $ = require('jquery');
 
 function Player(config){
   config = config || {};
+  $.extend(this,config);
   this.name= config.name;
   this.health=config.health;
   this.powers=config.powers;
 }
 
-function Enemy(config){
-  Player.call(config);
+function Enemy(name,health, powers){
+  Player.call(legionOfDoom);
+
+  var legionOfDoom = [
+    new Enemy({'name': "Lex Luthor", 'health': 10, 'powers':'brains'}),
+    new Enemy({'name': "The Joker", 'health': 10, 'powers':'poisonous gas'}),
+    new Enemy({'name': "Cheetah", 'health': 10, 'powers':'claws'}),
+    new Enemy({'name': "Black Manta", 'health': 10, 'powers':'energy blast'}),
+    new Enemy({'name': "Sinestro", 'health': 10, 'powers':'yellow power ring'}),
+    new Enemy({'name': "Zoom", 'health': 10, 'powers':'phase attack'}),
+  ];
+  console.log(legionOfDoom);
 }
 
-var legionOfDoom = [
-  new Enemy({'name': "Lex Luthor", 'health': 10, 'powers':'brains'}),
-  new Enemy({'name': "The Joker", 'health': 10, 'powers':'poisonous gas'}),
-  new Enemy({'name': "Cheetah", 'health': 10, 'powers':'claws'}),
-  new Enemy({'name': "Black Manta", 'health': 10, 'powers':'energy blast'}),
-  new Enemy({'name': "Sinestro", 'health': 10, 'powers':'yellow power ring'}),
-  new Enemy({'name': "Zoom", 'health': 10, 'powers':'phase attack'}),
-];
 
 
-console.log(legionOfDoom);
+
+console.log(Enemy());
 module.exports = {
   // 'Player' : Player
 
