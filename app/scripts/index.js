114fbf4effdf35954f6990a58371b4fc86508f69
Kara Mansel <callmedevmaybe@gmail.com>
2016-06-17 13:01:49 -0400

    added jquery on intro and heros

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,17 +2,26 @@ var $ = require('jquery');
 var models = require('./models.js');
 
 // $(function(){
-// //Attack Button Code
-// // $('.attack-button').on('click', function(event) {
-// //   event.preventDefault();
-// //   if()
-//
-// //Create on load event for random enemy choice
-// });
-//
-//
-//
-//
-//
-//
+//Attack Button Code
+// $('.attack-button').on('click', function(event) {
+//   event.preventDefault();
+//   if()
+
+//Shows a welcome message and gives instructions-set timeout not working
+$(document).ready(function(){
+
+    $('.message').text('Welcome to Injustice! Please choose a player from the Justice League.');
+
+});
+
+$('.good-guys').on('change', function(){
+  models.choosePlayer();
+});
+
+
+
+
+
+
+
 // });
--- app/scripts/models.js
+++ app/scripts/models.js
@@ -1,46 +1,73 @@
 var $ = require('jquery');
 
-function Player(config){
-  config = config || {};
-  $.extend(this,config);
-  this.name= config.name;
-  this.health=config.health;
-  this.powers=config.powers;
+function choosePlayer(){
+ var goodGuys = $('.good-guys');
+ var hero = (goodGuys.val());
+ console.log(hero); //gets the value of the chosen good guy
+  $('.hero-text').text('You have chosen ' + hero +'!' + ' Now ATTACK!');
 }
+// 
+// function Player(config){
+//   config = config || {};
+//   $.extend(this,config);
+//   this.name= config.name;
+//   this.health=config.health;
+//   this.powers=config.powers;
+// }
+//
+// // function Enemy(name,health, powers){
+// //   Player.call(legionOfDoom);
+// //
+// //   var legionOfDoom = [
+// //     new Enemy({'name': "Lex Luthor", 'health': 10, 'powers':'brains'}),
+// //     new Enemy({'name': "The Joker", 'health': 10, 'powers':'poisonous gas'}),
+// //     new Enemy({'name': "Cheetah", 'health': 10, 'powers':'claws'}),
+// //     new Enemy({'name': "Black Manta", 'health': 10, 'powers':'energy blast'}),
+// //     new Enemy({'name': "Sinestro", 'health': 10, 'powers':'yellow power ring'}),
+// //     new Enemy({'name': "Zoom", 'health': 10, 'powers':'phase attack'}),
+// //   ];
+// //   console.log(legionOfDoom);
+// // }
+// //
+//
+//
+//
+// function Hero(name,health, powers){
+//   Player.call(justiceLeague);
+//
+//   var justiceLeague = [
+//     new Hero({'name': "Superman", 'health': 10, 'powers':'heat vision, super strength, invulnerability'}),
+//     new Hero({'name': "Batman", 'health': 10, 'powers':'super awesome ninja skills, batarangs, sidekick assist'}),
+//     new Hero({'name': "Wonder Woman", 'health': 10, 'powers':'super strength, lasso of truth, deflect with indestructible bracelets'}),
+//     new Hero({'name': "Aquaman", 'health': 10, 'powers':'trident of neptune, super strength, telepathic shark attack'}),
+//     new Hero({'name': "Green Lantern", 'health': 10, 'powers':'energy blast, mental construct attact, force field'}),
+//     new Hero({'name': "The Flash", 'health': 10, 'powers':'super speed, vortex attack, speed force punch'}),
+//   ];
+//   console.log(legionOfDoom);
+// }
+//
+// function Enemy(name,health, powers){
+//   Player.call(legionOfDoom);
+//
+//   var legionOfDoom = [
+//     new Enemy({'name': "Lex Luthor", 'health': 10, 'powers':'outsmart enemy'}),
+//     new Enemy({'name': "The Joker", 'health': 10, 'powers':'poisonous gas'}),
+//     new Enemy({'name': "Cheetah", 'health': 10, 'powers':'claws'}),
+//     new Enemy({'name': "Black Manta", 'health': 10, 'powers':'atlantean attack'}),
+//     new Enemy({'name': "Sinestro", 'health': 10, 'powers':'yellow power ring'}),
+//     new Enemy({'name': "Zoom", 'health': 10, 'powers':'phase attack'}),
+//   ];
+//   console.log(legionOfDoom);
+// }
+//
+//
+//
+//
+// console.log(Enemy());
 
-function Hero(name,health, powers){
-  Player.call(justiceLeague);
+//
 
-  var justiceLeague = [
-    new Hero({'name': "Superman", 'health': 10, 'powers':'heat vision, super strength, invulnerability'}),
-    new Hero({'name': "Batman", 'health': 10, 'powers':'super awesome ninja skills, batarangs, sidekick assist'}),
-    new Hero({'name': "Wonder Woman", 'health': 10, 'powers':'super strength, lasso of truth, deflect with indestructible bracelets'}),
-    new Hero({'name': "Aquaman", 'health': 10, 'powers':'trident of neptune, super strength, telepathic shark attack'}),
-    new Hero({'name': "Green Lantern", 'health': 10, 'powers':'energy blast, mental construct attact, force field'}),
-    new Hero({'name': "The Flash", 'health': 10, 'powers':'super speed, vortex attack, speed force punch'}),
-  ];
-  console.log(legionOfDoom);
-}
-
-function Enemy(name,health, powers){
-  Player.call(legionOfDoom);
-
-  var legionOfDoom = [
-    new Enemy({'name': "Lex Luthor", 'health': 10, 'powers':'outsmart enemy'}),
-    new Enemy({'name': "The Joker", 'health': 10, 'powers':'poisonous gas'}),
-    new Enemy({'name': "Cheetah", 'health': 10, 'powers':'claws'}),
-    new Enemy({'name': "Black Manta", 'health': 10, 'powers':'atlantean attack'}),
-    new Enemy({'name': "Sinestro", 'health': 10, 'powers':'yellow power ring'}),
-    new Enemy({'name': "Zoom", 'health': 10, 'powers':'phase attack'}),
-  ];
-  console.log(legionOfDoom);
-}
-
-
-
-
-console.log(Enemy());
 module.exports = {
-  // 'Player' : Player
+'choosePlayer' : choosePlayer
 
 };
