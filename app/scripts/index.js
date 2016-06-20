0c547d9975bdb9a86922e4ae616996884b7fb739
Kara Mansel <callmedevmaybe@gmail.com>
2016-06-20 10:14:45 -0400

    Show Health at 20 at game start

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -20,12 +20,12 @@ $(function(){
   console.log(randomHeroPower);
 
   var legionOfDoom = [
-    new models.Enemy({name: 'Lex Luthor', health: 10, power: 'outsmarts you with his superior intelligence'}),
-    new models.Enemy({name: 'The Joker', health: 10, power: 'sprays you with poisonous gas'}),
-    new models.Enemy({name: 'Cheetah', health: 10, power: 'gouges you with her claws'}),
-    new models.Enemy({name: 'Black Manta', health: 10, power: 'sends his minions to attack you'}),
-    new models.Enemy({name: 'Sinestro', health: 10, power: 'attacks you with yellow power ring constructs'}),
-    new models.Enemy({name: 'Captain Cold', health: 10, power: 'freezes you with cold gun'}),
+    new models.Enemy({name: 'Lex Luthor', health: 20, power: 'outsmarts you with his superior intelligence'}),
+    new models.Enemy({name: 'The Joker', health: 20, power: 'sprays you with poisonous gas'}),
+    new models.Enemy({name: 'Cheetah', health: 20, power: 'gouges you with her claws'}),
+    new models.Enemy({name: 'Black Manta', health: 20, power: 'sends his minions to attack you'}),
+    new models.Enemy({name: 'Sinestro', health: 20, power: 'attacks you with yellow power ring constructs'}),
+    new models.Enemy({name: 'Captain Cold', health: 20, power: 'freezes you with cold gun'}),
   ];
 
 
@@ -34,6 +34,7 @@ $(function(){
    * this function is an event handler that runs the function "choosePlayer" and
    * hides the drop down list once a hero is selected.
    */
+
   $('.justice-league').on('change', function(){
     choosePlayer();
     window.setTimeout(function(){
@@ -41,6 +42,7 @@ $(function(){
         choosePlayer();
       });
     }, 1000);
+
   });
 
 
@@ -69,8 +71,10 @@ $(function(){
       $('.message-to-player').text('ATTACK!');
     }, 10000);
     window.setTimeout(function(){
-      $('.message-to-player').text('You used '+ randomHeroPower + ' !');
-    }, 15000);
+    $('.hero-health-status').text('Your Health: ' + selectedHeroHealth);
+    $('.villian-health-status').text('Villian Health: ' + currentVillianHealth);
+  }, 11000);
+
   }
 
   /*
@@ -87,25 +91,30 @@ $(function(){
   });
 
 
-  var selectedHeroHealth = 10;
+  var selectedHeroHealth = 20;
+  var currentVillianHealth = randomlyChooseVillian.health;
 
   function attackGenerator(){
     var attackValue = _.random(10);
     return attackValue;
   }
 
+//Need health NOT to go below 0-code below not working:
+// randomlyChooseVillian.health(Math.max(randomlyChooseVillian.health, 0));
+// selectedHeroHealth(Math.max(selectedHeroHealth, 0));
+
     // Decrease health value when attack-button is clicked
   $('.attack-button').on('click', function(){
     event.preventDefault();
-    var currentHeroHealth = selectedHeroHealth - attackGenerator();
-    $('.hero-health-status').text('Your Health: ' + currentHeroHealth);
-    console.log(currentHeroHealth);
+    selectedHeroHealth = selectedHeroHealth - attackGenerator();
+    $('.hero-health-status').text('Your Health: ' + selectedHeroHealth);
+    console.log(selectedHeroHealth);
 
     var currentVillianHealth = randomlyChooseVillian.health = randomlyChooseVillian.health - attackGenerator();
     $('.villian-health-status').text('Villian Health: ' + currentVillianHealth);
     console.log(currentVillianHealth);
 
-    if(currentHeroHealth <= 0){
+    if(selectedHeroHealth <= 0){
       alert('The Villian Wins!  Try Again.');
     } else if (currentVillianHealth <= 0){
       alert('Congratulations! You won!');
@@ -114,6 +123,10 @@ $(function(){
     window.setTimeout(function(){
       $('.message-to-player').text('The villian '+ randomlyChooseVillian.power + '.');
     }, 2000);
+
+    window.setTimeout(function(){
+      $('.message-to-player').text('You used '+ randomHeroPower + ' !');
+    }, 4000);
   });
 
 });
